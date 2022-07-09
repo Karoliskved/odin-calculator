5be0defd0d3bc3f0057fc3efc66d3cf51f582336
Karoliskved <karolis.kvedaravicius@mif.stud.vu.lt>
2022-07-09 21:20:24 +0300

    add equal, backspace, clear functions to the calculator

--- script.js
+++ script.js
@@ -1,79 +1,93 @@
 let display=document.querySelector(".input");
 let prevOp=undefined;
-let prevVal;
-let currentVal;
+let prevVal=undefined;
+let currentVal=undefined;
 let inputStatus=true;
-var testing=0
-let op
+let numberEntered=false;
 
 function add(a, b){
     return parseInt(a)+parseInt(b)
 }
 function subtract(a, b){
+
     return parseInt(a)-parseInt(b)
 }
 function mult(a, b){
     return parseInt(a)*parseInt(b)
 }
 function divide(a, b){
-    return parseInt(a)/parseInt(b)
+   
+    if(b==0)
+    return "aint that funny"
+    else
+    return parseFloat(a)/parseFloat(b)
 }
 function updateDis(string){
-    if(inputStatus==true)
-    display.innerText=display.innerText+string;
+    if(inputStatus && display.textContent!=="0")
+    display.textContent=display.textContent+string;
     else{
-         inputStatus=true;
-        display.innerText=string;
+        inputStatus=true;
+        display.textContent=string;
     }
 
 }
 function handleOperator(op){
-        
         prevVal=currentVal
-        currentVal=display.innerText;
+        currentVal=display.textContent;
        if(currentVal!==undefined && prevVal!==undefined){
         currentVal=  operate(prevOp, prevVal, currentVal)
         prevVal=undefined
-        
        }
        prevOp=op;
-        inputStatus=false
-    
-    console.log("previous val "+prevVal)
-    console.log("current " +currentVal)
-
-    console.log(op)
-   
-    
+        inputStatus=false 
+        numberEntered=false
 }
 function operate(prevOp, prevVal, currentVal){
-
     switch(prevOp)
         {
             case "/":
                 currentVal=divide(prevVal, currentVal)
-                display.innerText=currentVal
+              
+                display.textContent=currentVal
                 return currentVal
                 break;
             case "*":
                 currentVal=mult(prevVal, currentVal)
-                display.innerText=currentVal
+                display.textContent=currentVal
+
                 return currentVal
                 break; 
             case "-":
                 currentVal=subtract(prevVal, currentVal)
-                display.innerText=currentVal
+                display.textContent=currentVal
                 return currentVal
                 break;
             case "+":
                 currentVal=add(prevVal, currentVal)
-                display.innerText=currentVal
+                display.textContent=currentVal
                 return currentVal
                 break;
         }
+}
+function handleEqual(){
+    prevVal=currentVal
+    currentVal=display.textContent;
+    operate(prevOp, prevVal, currentVal)
+
+    currentVal=undefined
 
 }
-
+function handleBackspace(){
+    if(display.textContent!=="0")
+    display.textContent=display.textContent.slice(0, -1)
+}
+function handleClear(){
+    display.textContent=0
+    prevOp=undefined;
+    prevVal=undefined;
+    currentVal=undefined;
+    inputStatus=true;
+}
 
 
 const  buttons = document.querySelectorAll('.number')
@@ -88,13 +102,11 @@ operators.forEach(operator=>{
     operator.addEventListener('click', ()=>{handleOperator(operator.innerHTML)})
 }
 )
+const  clear = document.querySelector('.clear')
+clear.addEventListener('click', ()=>{handleClear()})
+const  backspace = document.querySelector('.backspace')
+backspace.addEventListener('click', ()=>{handleBackspace()})
+const  equal = document.querySelector('.equal')
+equal.addEventListener('click', ()=>{handleEqual()})
 
-/*const btns = document.querySelectorAll('button')
 
-btns.forEach(btn => {
-
-   btn.addEventListener('click', event => {
-        console.log( event.target.id );
-   });
-
-});*/
